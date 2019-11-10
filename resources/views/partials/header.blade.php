63a9921621f51b3c297d139d0b470356eecc6f2e
alfinm01 <arrestral@gmail.com>
2019-11-11 03:25:36 +0700

    Add Bootstrap

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,3 @@
-<a href="{{ url('/') }}" target="_blank" class="logo"style="font-size: 18px;">
-    <b>Loofow</b>
-</a>
-
-<nav class="navbar navbar-static-top" role="navigation">
+<nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="{{ url('/') }}">Loofow</a>
 </nav>
