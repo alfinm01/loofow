da97ca1ad2a26c5b5fbef952a5bc96c245fa2f8a
alfinm01 <arrestral@gmail.com>
2019-11-21 06:50:37 +0700

    Update footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,17 +1,10 @@
-
-	<footer class="footer">
-	    <div class="container">
-	        <div class="content has-text-centered">
-	            <div class="soc">
-	                <a href="#"><i class="fa fa-github-alt fa-2x" aria-hidden="true"></i></a>
-	                <a href="#"><i class="fa fa-youtube fa-2x" aria-hidden="true"></i></a>
-	                <a href="#"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a>
-	                <a href="#"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
-	            </div>
-	            <p>
-	                <strong>Bulma</strong> by <a href="http://jgthms.com">Jeremy Thomas</a>.
-	                The source code is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. <br>
-	            </p>
-	        </div>
-	    </div>
-	</footer>
+<footer class="footer">
+    <div class="container">
+        <div class="content has-text-centered">
+            <p>
+                <strong>Loofow</strong> by <a href="https://github.com/alfinm01/loofow">Kelompok 8 RPL STI</a> <br>
+                Copyright 2019 <br>
+            </p>
+        </div>
+    </div>
+</footer>
