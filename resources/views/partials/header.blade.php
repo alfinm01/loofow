da88abe885489238846d7fe2ac80eae0bd0bc9d7
alfinm01 <arrestral@gmail.com>
2019-11-21 06:39:06 +0700

    Update header partials

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,55 +3,59 @@
 	<nav class="navbar">
 	    <div class="container">
 	        <div class="navbar-brand">
-	            <a class="navbar-item" href="../">
-	                    <img src="../images/bulma.png" alt="Logo">
-	                </a>
+	            <a class="navbar-item" href="{{url('/')}}">
+                    Loofow
+                </a>
 	            <span class="navbar-burger burger" data-target="navbarMenu">
-	                    <span></span>
+	            <span></span>
 	            <span></span>
 	            <span></span>
 	            </span>
 	        </div>
 	        <div id="navbarMenu" class="navbar-menu">
 	            <div class="navbar-end">
+	            	<!-- Authentication Links -->
 	                <a href="{{url('/')}}" class="navbar-item is-active">
 	                        Home
 	                    </a>
-	                <a class="navbar-item is-active" href="{{url('/dashboard')}}">
-	                        Dashboard
+	                <a href="{{url('/search')}}" class="navbar-item is-active">
+	                        Search
 	                    </a>
-	                <a class="navbar-item is-active" href="{{url('/create-post')}}">
-	                        Post
-	                    </a>
+	                @guest
 	                <a class="navbar-item is-active" href="{{url('/login')}}">
 	                        Login
 	                    </a>
 	                <a class="navbar-item is-active" href="{{url('/register')}}">
 	                        Register
 	                    </a>
-	                <a class="navbar-item">
-	                        Help
-	                    </a>
+	                @else
 	                <div class="navbar-item has-dropdown is-hoverable">
 	                    <a class="navbar-link">
-	                            Account
+	                            {{ Auth::user()->name }}
 	                        </a>
 	                    <div class="navbar-dropdown">
-	                        <a class="navbar-item">
+	                        <a class="navbar-item" href="{{url('/dashboard')}}">
 	                                Dashboard
 	                            </a>
-	                        <a class="navbar-item">
+	                        <a class="navbar-item" href="{{url('/profile')}}">
 	                                Profile
 	                            </a>
-	                        <a class="navbar-item">
-	                                Settings
+	                        <a class="navbar-item" href="{{url('/create-post')}}">
+	                                Create Post
 	                            </a>
 	                        <hr class="navbar-divider">
-	                        <div class="navbar-item">
-	                            Logout
-	                        </div>
+	                        <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
 	                    </div>
 	                </div>
+	                @endguest
 	            </div>
 	        </div>
 	    </div>
