f6e4adcd9d22cc88e59ad796a6ce0969975223cc
luthfiihakiim <18217018@std.stei.itb.ac.id>
2019-11-22 20:03:15 +0700

    List Post Update

--- app/Http/Controllers/Post/ListPost.php
+++ app/Http/Controllers/Post/ListPost.php
@@ -11,28 +11,13 @@ use Auth;
 class ListPost extends Controller {
     public function listAllItems() {
         //ambil data
-        $post = DB::table('posts')->get();
+        $postAsc = DB::table('posts')->orderBy('year', 'asc')->orderBy('month', 'asc')->orderBy('date', 'asc')->get();
+
+        $postDsc = DB::table('posts')->orderBy('year', 'desc')->orderBy('month', 'desc')->orderBy('date', 'desc')->get();
 
         //pass to view -- Asumsi readpost dilakuin di bagian home
-        return view('home', ['posts' => $post]);
+        return view('home', ['postsAsc' => $postAsc, 'postsDsc' => $postDsc]);
     }
-
-    public function listAllItemsAscending() {
-        //ambil data
-        $post = DB::table('posts')->orderBy('year', 'asc')->orderBy('month', 'asc')->orderBy('date', 'asc')->get();
-
-        //pass to view -- Asumsi readpost dilakuin di bagian home
-        return view('home', ['posts' => $post]);
-    }
-
-    public function listAllItemsDescending() {
-        //ambil data
-        $post = DB::table('posts')->orderBy('year', 'desc')->orderBy('month', 'desc')->orderBy('date', 'desc')->get();
-
-        //pass to view -- Asumsi readpost dilakuin di bagian home
-        return view('home', ['posts' => $post]);
-    }
-
 
     public function listByKeywords(Request $request) {
         //ambil data dengan keyword tertentu
@@ -44,25 +29,11 @@ class ListPost extends Controller {
 
     public function listByUser() {
         //ambil data berdasarkan user_id
-        $post = DB::table('posts')->where('user_id', Auth::id())->get();
-
+        $postFound = DB::table('posts')->where('user_id', Auth::id())->where('type', '=', 'Found')->orderBy('year', 'asc')->orderBy('month', 'asc')->orderBy('date', 'asc')->get();
+        
+        $postLost = DB::table('posts')->where('user_id', Auth::id())->where('type', '=', 'Lost')->orderBy('year', 'asc')->orderBy('month', 'asc')->orderBy('date', 'asc')->get();
+        
         //Menampilkan hasil data dengan keywords tertentu
-        return view('dashboard',['posts' => $post]);
-    }
-
-    public function listByUserFound() {
-        //ambil data berdasarkan user_id
-        $post = DB::table('posts')->where('user_id', Auth::id())->where('type', '=', 'Found')->get();
-
-        //Menampilkan hasil data dengan keywords tertentu
-        return view('dashboard',['posts' => $post]);
-    }
-
-    public function listByUserLost() {
-        //ambil data berdasarkan user_id
-        $post = DB::table('posts')->where('user_id', Auth::id())->where('type', '=', 'Lost')->get();
-
-        //Menampilkan hasil data dengan keywords tertentu
-        return view('dashboard',['posts' => $post]);
+        return view('dashboard',['postsFound' => $postFound, 'postsLost' => $postLost]);
     }
 }
--- resources/views/pages/create-post.blade.php
+++ resources/views/pages/create-post.blade.php
@@ -171,7 +171,7 @@
 						<label class="label">Upload Foto Barang</label>
 						<div class="file is-primary">
 							<label class="file-label">
-							<input class="file-input" type="file" name="foto">
+							<input class="file-input" type="file" name="image">
 							<span class="file-cta">
 								<span class="file-label">
 								Upload
